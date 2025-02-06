401678e76a729cb748a5ca71a84d40ca4a7549c2
tanishq45632 <tanishqsinghal483@gmail.com>
2025-02-06 11:54:31 +0530

    added

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-const express = require('express');
-const { resolve } = require('path');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
-const port = 3010;
+const PORT = process.env.PORT || 8000;
 
-app.use(express.static('static'));
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.error(`Error connecting to database: ${err.message}`));
 
-app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+// Middleware
+app.use(express.json());
+
+// Basic RoutePort = 8000
+DB_URL = "mongodb+srv://tanishq12:tanishq123@cluster0.gtf9s.mongodb.net/"
+app.get("/", (req, res) => {
+  res.send("Customer Management System Backend is Running");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
+
+
